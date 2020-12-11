62e68a27573c2eb8511e631ab93f838760ba35b5
itpstudent <itpstudent@Prod0012.lan>
2020-12-11 10:00:07 -0500

    serialcommunication

--- sketch.js
+++ sketch.js
@@ -1,13 +1,71 @@
+//initialising serial
+var serial;          // variable to hold an instance of the serialport library
+var portName = '/dev/tty.usbmodem1421'; // fill in your serial port name here
+var inData;                            // for incoming serial data
+var outByte = 0; 
+let serialOptions = { baudrate: 9600};
 let video;
 let poseNet;
 let pose;
 let skeleton;
-
 let brain;
 let poseLabel = "";
-
+let poseMapping = {
+  'moveLeft':2,
+  'moveRight':3,
+  'drawLongLeft':5,
+  'drawLongRight':4,
+  'drawShortRight':6,
+  'drawShortLeft':7,
+  'drawBothLeft':8,
+  'drawBothRight':9,
+  'moveLeftRepeat':10,
+  'moveRightRepeat':11
+}
+// get the list of ports:
+function printList(portList) {
+  // portList is an array of serial port names
+  for (var i = 0; i < portList.length; i++) {
+    // Display the list the console:
+    console.log(i + portList[i]);
+  }
+}
+function serverConnected() {
+  console.log('connected to server.');
+}
+ 
+function portOpen() {
+  console.log('the serial port opened.')
+}
+ 
+function serialEvent() {
+  var inString = serial.readLine();
+  if (inString.length > 0 ) {
+    // convert it to a number:
+    inData = Number(inString);
+    console.log('NUM',inData);
+  }
+}
+ 
+function serialError(err) {
+  console.log('Something went wrong with the serial port. ' + err);
+}
+ 
+function portClose() {
+  console.log('The serial port closed.');
+}
 function setup() {
   createCanvas(640, 480);
+  serial = new p5.SerialPort();    // make a new instance of the serialport library
+  serial.on('list', printList);  // set a callback function for the serialport list event
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen);        // callback for the port opening
+  serial.on('data', serialEvent);     // callback for when new data arrives
+  serial.on('error', serialError);    // callback for errors
+  serial.on('close', portClose);      // callback for the port closing
+  serial.list();  
+  serial.open(portName,serialOptions); 
+
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
@@ -100,5 +158,8 @@ function draw() {
   noStroke();
   textSize(70);
   textAlign(CENTER, CENTER);
+  if(poseMapping[poseLabel]) {
+    serial.write(poseMapping[poseLabel]);
+  }
   text(poseLabel, width / 2, height / 2);
 }
